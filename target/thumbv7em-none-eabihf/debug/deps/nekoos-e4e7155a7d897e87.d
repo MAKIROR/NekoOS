831db4d8c666f5d40d3b397ef6fb3d849f5d0e6b
MAKIROR <gzanan@gmail.com>
2022-11-11 08:48:28 -0500

    Update

--- target/thumbv7em-none-eabihf/debug/deps/nekoos-e4e7155a7d897e87.d
+++
@@ -1,5 +0,0 @@
-/home/makiror/work/NekoOS/target/thumbv7em-none-eabihf/debug/deps/nekoos-e4e7155a7d897e87: src/main.rs
-
-/home/makiror/work/NekoOS/target/thumbv7em-none-eabihf/debug/deps/nekoos-e4e7155a7d897e87.d: src/main.rs
-
-src/main.rs:
--- target/thumbv7em-none-eabihf/debug/nekoos.d
+++
@@ -1,1 +0,0 @@
-/home/makiror/work/NekoOS/target/thumbv7em-none-eabihf/debug/nekoos: /home/makiror/work/NekoOS/src/main.rs

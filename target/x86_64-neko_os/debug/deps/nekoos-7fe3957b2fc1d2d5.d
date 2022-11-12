34a024ca5eaf4e70b4abc9a6f5f1f79c606bc64b
MAKIROR <gzanan@gmail.com>
2022-11-12 21:03:59 +0800

    Update

---
+++ target/x86_64-neko_os/debug/deps/nekoos-7fe3957b2fc1d2d5.d
@@ -0,0 +1,5 @@
+/home/makiror/works/NekoOS/target/x86_64-neko_os/debug/deps/nekoos-7fe3957b2fc1d2d5: src/main.rs
+
+/home/makiror/works/NekoOS/target/x86_64-neko_os/debug/deps/nekoos-7fe3957b2fc1d2d5.d: src/main.rs
+
+src/main.rs:
---
+++ target/x86_64-neko_os/debug/nekoos.d
@@ -0,0 +1,1 @@
+/home/makiror/works/NekoOS/target/x86_64-neko_os/debug/nekoos: /home/makiror/works/NekoOS/src/main.rs

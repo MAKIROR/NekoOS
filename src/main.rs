2d6f3f23a237fa6571757f5afa87fbcf1541337d
MAKIROR <gzanan@gmail.com>
2022-11-13 21:41:43 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@
 
 extern crate bootloader;
 
+#[path = "mods/vga_buffer.rs"]
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
@@ -16,14 +18,7 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    println!("Hello World{}", "!");
 
     loop {}
 }

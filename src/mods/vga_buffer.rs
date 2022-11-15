89772a337d2f19957c1d7264f2cc079e628e0f0f
MAKIROR <gzanan@gmail.com>
2022-11-15 20:38:27 +0800

    Remove something

--- src/mods/vga_buffer.rs
+++ src/mods/vga_buffer.rs
@@ -137,16 +137,3 @@ pub fn _print(args: fmt::Arguments) {
     use core::fmt::Write;
     WRITER.lock().write_fmt(args).unwrap();
 }
-
-pub fn print_something() {
-    use core::fmt::Write;
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Yellow, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
-
-    writer.write_byte(b'H');
-    writer.write_string("ello! ");
-    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
-}

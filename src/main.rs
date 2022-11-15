4647828c53c35184c0ad1d13550cd130694cf349
MAKIROR <gzanan@gmail.com>
2022-11-15 21:11:38 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![feature(panic_handler)]
 #![feature(core_intrinsics)]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 #![no_std]
 #![no_main]
 
@@ -16,8 +20,26 @@ fn panic(info: &PanicInfo) -> ! {
 }
 
 #[no_mangle]
-pub extern "C" fn _start() {
+pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }

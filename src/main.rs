2ab8698790d98c1ca3589207affa872bf7a0633e
MAKIROR <gzanan@gmail.com>
2022-11-24 14:52:44 +0800

    Integration Tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,11 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(neko_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-extern crate bootloader;
-
-#[path = "mods/vga_buffer.rs"]
-mod vga_buffer;
-#[path = "mods/serial.rs"]
-mod serial;
 use core::panic::PanicInfo;
-
-// our panic handler in test mode
-#[cfg(test)]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
+use neko_os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -32,46 +17,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-pub fn test_runner(tests: &[&dyn Testable]) { // new
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run(); // new
-    }
-    exit_qemu(QemuExitCode::Success);
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    loop {}
 }
 
-#[test_case]
-fn trivial_assertion() {
-    assert_eq!(1, 1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    neko_os::test_panic_handler(info)
 }

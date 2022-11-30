09dba3117828871fd9c4f10fe225a6326b9efaaf
MAKIROR <gzanan@gmail.com>
2022-11-30 15:00:42 +0800

    Async/Await

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub mod interrupts;
 pub mod gdt;
 pub mod memory;
 pub mod allocator;
+pub mod task;
 
 #[cfg(test)]
 use bootloader::{entry_point, BootInfo};
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,10 @@
 #![reexport_test_harness_main = "test_main"]
 
 extern crate alloc;
-
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use neko_os::println;
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use neko_os::task::{Task,keyboard,executor::Executor};
 
 entry_point!(kernel_main);
 
@@ -28,34 +27,13 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
-    // allocate a number on the heap
-    let heap_value = Box::new(41);
-    println!("heap_value at {:p}", heap_value);
-
-    // create a dynamically sized vector
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    // create a reference counted vector -> will be freed when count reaches 0
-    let reference_counted = Rc::new(vec![1, 2, 3]);
-    let cloned_reference = reference_counted.clone();
-    println!(
-        "current reference count is {}",
-        Rc::strong_count(&cloned_reference)
-    );
-    core::mem::drop(reference_counted);
-    println!(
-        "reference count is {} now",
-        Rc::strong_count(&cloned_reference)
-    );
     #[cfg(test)]
     test_main();
 
-    println!("It did not crash!");
-    neko_os::hlt_loop();
+    let mut executor = Executor::new();
+    executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses()));
+    executor.run();
 }
 
 #[cfg(not(test))]
@@ -74,4 +52,12 @@ fn panic(info: &PanicInfo) -> ! {
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
+}
+async fn async_number() -> u32 {
+    42
+}
+
+async fn example_task() {
+    let number = async_number().await;
+    println!("async number: {}", number);
 }
